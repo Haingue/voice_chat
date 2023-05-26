247c8f9a73a6be3cf58c5f41ab79ea6635c2c825
haingue <fabien.haingue@gmail.com>
2023-05-26 17:37:09 +0200

    FIX: TTS special characters

--- src/text_to_speech/tts.py
+++ src/text_to_speech/tts.py
@@ -4,7 +4,10 @@ model_name = "tts_models/fr/mai/tacotron2-DDC"
 
 
 def read_text(input_text: str):
-    # input_text = input_text.strip().replace("\"", "'")
+    input_text = input_text.replace("\"", "'")
+    input_text = input_text.replace("\r\n", " ")
+    input_text = input_text.replace("\n", " ")
+    input_text = input_text.strip()
     cmd = "tts --text \"%s\" --model_name \"%s\" >.\\env\\tts.log" % (input_text, model_name)
     print("CMD: %s" % cmd)
     os.system(cmd)
